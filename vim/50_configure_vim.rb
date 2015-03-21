d09c0fde4ce718f3e87c767b4c5ef190311098fa
Cory Logan <he@corylogan.com>
2015-03-21 14:44:38 -0700

    adding option for pull

--- vim/50_configure_vim.rb
+++ vim/50_configure_vim.rb
@@ -1,10 +1,29 @@
 #! /usr/bin/env ruby
 
 puts "Cloning the vim config into ~/.vim"
-`git clone --recursive https://github.com/counterbeing/Vim-Configuration #{HOME}/.vim`
-make_link_where_appropriate("#{HOME}/.vim/.vimrc", "#{HOME}/.vimrc")
+
+vim_folder = "#{HOME}/.vim"
+git_config = vim_folder + "/.git/config"
+
+def run_clone
+  `git clone --recursive https://github.com/counterbeing/Vim-Configuration #{HOME}/.vim`
+end
+
+if File.exist?(git_config) && File.read(git_config).scan(/counterbeing\/Vim-Configuration/)
+  Dir.chdir(vim_folder) do
+    `git pull`
+  end
+elsif Dir.exist?(vim_folder)
+  backup(vim_folder)
+  run_clone
+else
+  run_clone
+end
+
+make_link_where_appropriate("#{vim_folder}/.vimrc", "#{HOME}/.vimrc")
 
 puts "Installing vim bundles with Vundle, this will take a moment"
-Dir.chdir("#{HOME}/.vim") do
+Dir.chdir(vim_folder) do
+  # Install all Vundle plugins
   %x[vim +PluginInstall +qall]
 end
