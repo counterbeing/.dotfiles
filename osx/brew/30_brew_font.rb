1e44446194cc12b66b6c75b9c9a5ab20dc618b00
Cory Logan <clogan@cloudpassage.com>
2016-04-26 10:43:29 -0700

    modifiy install

--- osx/brew/30_brew_font.rb
+++ osx/brew/30_brew_font.rb
@@ -1,18 +1,18 @@
-# puts "#{Tty.blue}Installing fonts...#{Tty.reset}"
-#
-# recipes = [
-#   'font-meslo-lg-for-powerline',  # Used in terminal
-#   'font-m-plus',
-#   'font-clear-sans',
-#   'font-roboto'
-# ]
-#
-# recipes.each do |r|
-#   unless INSTALLED_CASK_RECIPES.include?(r)
-#     puts "    installing #{r}"
-#     `brew cask install #{r}`
-#   else
-#     puts "    skipping #{r}"
-#   end
-# end
-#
+puts "#{Tty.blue}Installing fonts...#{Tty.reset}"
+
+recipes = [
+  'font-meslo-lg-for-powerline',  # Used in terminal
+  'font-m-plus',
+  'font-clear-sans',
+  'font-roboto'
+]
+
+recipes.each do |r|
+  unless INSTALLED_CASK_RECIPES.include?(r)
+    puts "    installing #{r}"
+    `brew cask install #{r}`
+  else
+    puts "    skipping #{r}"
+  end
+end
+
