73685c31dcb55d55551d30d14be339c9f2b95058
Cory Logan <he@corylogan.com>
2016-07-07 13:18:19 -0400

    update package list

--- osx/atom/10_install_packages.rb
+++ osx/atom/10_install_packages.rb
@@ -4,4 +4,4 @@ puts "Installing packages from list #{packages_file}"
 
 system("apm install --packages-file #{packages_file}")
 system('apm upgrade')
-system("apm list -ib >> #{packages_file}")
+system("apm list -ib > #{packages_file}")
