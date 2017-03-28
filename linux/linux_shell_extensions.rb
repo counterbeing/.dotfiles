f20e4ebc51c63577faf2aa185e23e61263062350
Cory Logan <he@corylogan.com>
2017-03-28 18:24:57 +0000

    fixssh for tmux sessions

--- linux/linux_shell_extensions.rb
+++ linux/linux_shell_extensions.rb
@@ -25,7 +25,7 @@ class LinuxShellExtensions
 
   def call
     install_apt_packages(
-      %w(ack-grep pick jq git-crypt tmate mosh)
+      %w(ack-grep pick jq git-crypt tmate mosh gnupg-agent)
     )
     system(DIFF_SO_FANCY)
     system(FZF)
