64c38a6c815ddfdbc85ac3ee4fb4ee7a3fe388ab
Cory Logan <he@corylogan.com>
2022-01-17 14:45:24 -0800

    add a few bins

--- osx/brew/20_brew_cli.rb
+++ osx/brew/20_brew_cli.rb
@@ -2,25 +2,25 @@
 class BrewCli
   include Osx
   extend Osx
-  DEPENDENCIES = %w(brew_installer).freeze
+  DEPENDENCIES = %w[brew_installer].freeze
   MESSAGE = 'Installing Brew packages...'.freeze
 
   RECIPES = [
-    'ack',                        # Search inside of files
-    'ansible',                    # System configuration/management
+    'ack', # Search inside of files
+    'ansible', # System configuration/management
     'asciinema',
     'asdf',
-    'autoenv',                    # Automatically source .env files in any dir
+    'autoenv', # Automatically source .env files in any dir
     'awscli',
-    'bash',                       # Updated bash
+    'bash', # Updated bash
     'bfg',
     'cfssl',
-    'clamav',                     # Antivirus
+    'clamav', # Antivirus
     'clang-format',
-    'cmake',                      # Compiler
+    'cmake', # Compiler
     'cowsay',
-    'ctags',                      # Tagging for Vim
-    'curl',                       # Updated
+    'ctags', # Tagging for Vim
+    'curl', # Updated
     'diff-so-fancy',
     'direnv',
     'docker-clean',
@@ -28,23 +28,25 @@ class BrewCli
     'exiftool',
     'fd',
     'ffmpeg',
+    'exa',
+    'bat',
     'hyperfine',
     'fzf',
-    'ghostscript',                # PDF support for imagemagick
+    'ghostscript', # PDF support for imagemagick
     'git-crypt',
-    'git',                        # Version control
-    'gnupg',                      # For securing messages
+    'git', # Version control
+    'gnupg', # For securing messages
     'go',
     'gpg-agent',
     'heroku',
     'highlight',
-    'htop-osx',                   # Activity monitor
+    'htop-osx', # Activity monitor
     'httpie',
     'hub',
-    'hugo',
-    'imagemagick',                # Image manipulation
+    'imagemagick', # Image manipulation
     'jpeg',
-    'jq',                         # Parse json in bash
+    'jq', # Parse json in bash
+    'yq',
     'jwt-cli',
     'k9s',
     'kafkacat',
@@ -54,41 +56,41 @@ class BrewCli
     'libssh',
     'libssh2',
     'libtiff',
-    'mobile-shell',               # SSH like client for low quality connections aka mosh
-    'mtr',                        # Debugging network connections
-    'openssl',                    # Patched OpenSSL for latest security releases
+    'mobile-shell', # SSH like client for low quality connections aka mosh
+    'mtr', # Debugging network connections
+    'openssl', # Patched OpenSSL for latest security releases
     'pgcli',
-    'pick',                       # Search multiline strings fuzzily
+    'pick', # Search multiline strings fuzzily
     'pinentry-mac',
-    'postgresql',                 # Database for local servers
-    'python',                     # Recent python
+    'postgresql', # Database for local servers
+    'python', # Recent python
     'reattach-to-user-namespace', # Allows Tmux to interact with the system pasteboard via pbcopy etc
     'redis',
-    'ruby-build',                 # Build ruby versions for rbenv
+    'ruby-build', # Build ruby versions for rbenv
     'sassc',
-    'siege',                      # Load testing
+    'siege', # Load testing
     'speedtest_cli',
-    'ssh-copy-id',                # Copies ssh keys and puts in authorized on remote host
-    'terminal-notifier',          # Send notifications to notification center
+    'ssh-copy-id', # Copies ssh keys and puts in authorized on remote host
+    'terminal-notifier', # Send notifications to notification center
     'terraform',
-    'the_silver_searcher',        # Fast file searching
-    'thefuck',                    # Automagic command correction
-    'tig',                        # Beautiful git visualization
-    'tmate',                      # Easy remote pair sessions
-    'tmux-cssh',                  # SSH into multiple computers
-    'tmux',                       # Terminal multiplexer, great for pairing
+    'the_silver_searcher', # Fast file searching
+    'thefuck', # Automagic command correction
+    'tig', # Beautiful git visualization
+    'tmate', # Easy remote pair sessions
+    'tmux-cssh', # SSH into multiple computers
+    'tmux', # Terminal multiplexer, great for pairing
     'tmuxinator',
     'tor',
-    'tree',                       # Display filesystem as tree in terminal
-    'unar',                       # Unrar, that's all
+    'tree', # Display filesystem as tree in terminal
+    'unar', # Unrar, that's all
     'owenthereal/upterm/upterm',
-    'vim',                        # Best text editor ever
-    'wget',                       # Curl alternative, some things use it
+    'vim', # Best text editor ever
+    'wget', # Curl alternative, some things use it
     'youtube-dl',
-    'zlib',                       # Compression, useful for siege
-    'zsh-completions',            # Various zsh completions
-    'zsh',                        # Like bash, but way better
-    'zsh-syntax-highlighting'     # syntax highlighting as you type
+    'zlib', # Compression, useful for siege
+    'zsh-completions', # Various zsh completions
+    'zsh', # Like bash, but way better
+    'zsh-syntax-highlighting', # syntax highlighting as you type
   ].freeze
 
   def call
